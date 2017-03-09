f07208c0c1a32366154c8458295d668c97c8483b
TRUMPF\ElyGomesMa <marcel.elygomes@de.trumpf.com>
2017-03-09 14:15:58 +0100

    New route for paymentinvoice

---
+++ MarketplaceCore-app/schema/paymentinvoice_schema.js
@@ -0,0 +1,41 @@
+/* ##########################################################################
+ -- Author: Marcel Ely Gomes
+ -- Company: Trumpf Werkzeugmaschine GmbH & Co KG
+ -- CreatedAt: 2017-02-27
+ -- Description: Schema for TechnologyData
+ -- ##########################################################################*/
+
+var self = {};
+
+self.GetAll = {
+    type: 'object',
+    properties: {
+        userUUID: {
+            type: 'string',
+            required: true
+        }
+    }
+};
+
+self.GetSingle = {
+    type: 'object',
+    properties: {
+        userUUID: {
+            type: 'string',
+            required: true
+        }
+    }
+};
+
+self.SaveDataQuery = {
+    type: 'object',
+    userUUID: {
+        type: 'string',
+        required: true
+    }
+};
+
+self.saveTechnologyData = {};
+
+
+module.exports = self;
