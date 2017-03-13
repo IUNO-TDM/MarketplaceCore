6522eb7f284e4788cd8f29474205704f4a3546e9
Christian Görg <christian.goerg@de.trumpf.com>
2017-03-13 13:56:58 +0100

    Payment service added -not yet working - license update for hams id

--- MarketplaceCore-app/services/invoice_service.js
+++ MarketplaceCore-app/services/invoice_service.js
@@ -3,31 +3,54 @@
  */
 
 var queries = require('../connectors/pg-queries');
+
+var payment_service = require('./payment_service');
 var self = {};
 
-//TODO: Remove this function
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+
 
 self.generateInvoice = function (request,transaction, callback) {
+
+
     var invoice = {
         totalAmount: 100000 ,
         referenceId: transaction.otransactionuuid,
         expiration: new Date(new Date().getTime() + (2 * 60 * 60 * 1000)).toISOString(),
-        transfers: [
-
-            {
-                address: 'mqN3sB5jo3r4aDRmXHHQZMf9tCwP1zMzce',
-                coin: 70000
-            }
-        ]
+        transfers: []
     };
+    
+    payment_service.createLocalInvoice(invoice, function (e, invoice) {
+        
+        payment_service.getInvoiceTransfers(invoice, function (e,transfers) {
+            var res = {
+                totalAmount: invoice.totalAmount,
+                invoiceId: invoice.invoiceId,
+                referenceId: invoice.referenceId,
+                expiration: invoice.expiration,
+                transfers: transfers
+            };
+            callback(null,res);
+        });
+
+    });
+    
+
+    //
+    // var invoice = {
+    //     totalAmount: 100000 ,
+    //     referenceId: transaction.otransactionuuid,
+    //     expiration: new Date(new Date().getTime() + (2 * 60 * 60 * 1000)).toISOString(),
+    //     transfers: [
+    //
+    //         {
+    //             address: 'mqN3sB5jo3r4aDRmXHHQZMf9tCwP1zMzce',
+    //             coin: 70000
+    //         }
+    //     ]
+    // };
 
 
-    callback(null, invoice);
+
 };
 
 
