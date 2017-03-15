c1e09facccffe6ed08cc146d6c58f30e47030796
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2017-03-15 11:02:09 +0100

    Implemented image routes for technologydata and users

--- MarketplaceCore-app/routes/technologydata.js
+++ MarketplaceCore-app/routes/technologydata.js
@@ -66,4 +66,43 @@ router.post('/', validate({
     });
 });
 
+router.get('/:id', validate({query: require('../schema/technologydata_schema').GetSingle}), function (req, res, next) {
+
+    queries.GetTechnologyDataByID(req.query['userUUID'], req.params['id'], function (err, data) {
+        if (err) {
+            next(err);
+        }
+        else {
+            if (!data || !Object.keys(data).length) {
+                logger.info('No technology data found for id: ' + req.param['id']);
+                res.sendStatus(404);
+
+                return;
+            }
+
+            var imgPath = data.technologydataimgref;
+
+            if (imgPath) {
+                var fs = require('fs');
+
+                fs.readFile(imgPath, function (err, fileBuffer) {
+                    if (err) {
+                        logger.warn('Cannot read file from path: ' + imgPath);
+                        logger.warn(err);
+
+                        res.sendStatus(500);
+
+                        return;
+                    }
+
+                    res.set('Content-Type', 'image/jpg');
+                    res.send(fileBuffer);
+                });
+            }
+
+        }
+    });
+
+});
+
 module.exports = router;
