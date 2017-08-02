fb7958921a8d51b2d5e5ee5fc42f9f86d9846605
TRUMPF\ElyGomesMa <marcel.elygomes@de.trumpf.com>
2017-08-02 17:18:30 +0200

    Create Reports functions for user private usage

---
+++ MarketplaceCore-app/database/function/myreport.js
@@ -0,0 +1,70 @@
+/**
+ * Created by beuttlerma on 31.05.17.
+ */
+
+var logger = require('../../global/logger');
+var db = require('../db_connection');
+
+var self = {};
+
+self.GetActivatedLicensesSinceForUser = function (userUUID, roles, sinceDate, callback) {
+    db.func('GetActivatedLicensesSinceForUser',[sinceDate, userUUID, roles])
+        .then(function (data) {
+            logger.debug(data);
+            callback(null, data);
+        })
+        .catch(function (error) {
+            logger.crit(error);
+            callback(error);
+        });
+};
+
+self.GetTopTechnologyDataSinceForUser= function(userUUID, roles, sinceDate, topValue, callback){
+    db.func('GetTopTechnologyDataSinceForUser', [sinceDate, topValue, userUUID, roles])
+        .then(function (data) {
+            logger.debug(data);
+            callback(null, data);
+        })
+        .catch(function (error) {
+            logger.crit(error);
+            callback(error);
+        });
+};
+
+self.GetWorkloadSinceForUser = function(userUUID, roles, sinceDate, callback){
+    db.func('GetWorkloadSinceForUser', [sinceDate, userUUID, roles])
+        .then(function (data) {
+            logger.debug(data);
+            callback(null, data);
+        })
+        .catch(function (error) {
+            logger.crit(error);
+            callback(error);
+        });
+};
+
+self.GetMostUsedComponentsForUser = function(userUUID, roles, sinceDate, topValue, callback){
+    db.func('GetMostUsedComponentsForUser', [sinceDate, topValue, userUUID, roles])
+        .then(function (data) {
+            logger.debug(data);
+            callback(null, data);
+        })
+        .catch(function (error) {
+            logger.crit(error);
+            callback(error);
+        });
+};
+
+self.GetRevenueForUser = function(userUUID, roles, sinceDate, callback){
+    db.func('GetRevenueForUser', [sinceDate, userUUID, roles])
+        .then(function (data) {
+            logger.debug(data);
+            callback(null, data);
+        })
+        .catch(function (error) {
+            logger.crit(error);
+            callback(error);
+        });
+};
+
+module.exports = self;
