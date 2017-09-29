1bd832ca48a2427efdaf464837176cb2ab87cd60
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2017-09-29 13:11:05 +0200

    Refactored lcc adapter to use different strategies depending on the node_env. In case of NODE_ENV=docker all lcc calls are beeing skipped

---
+++ MarketplaceCore-app/adapter/license_central_strategy/default.js
@@ -0,0 +1,245 @@
+const logger = require('../../global/logger');
+const CONFIG = require('../../config/config_loader');
+const request = require('request');
+
+const fs = require('fs');
+const path = require('path');
+
+const p12File = path.resolve(__dirname, CONFIG.LICENSE_CENTRAL.CERT.P12_FILE_PATH);
+const caFile = path.resolve(CONFIG.TRUSTED_CA_FILE_PATH);
+
+const self = {};
+
+function buildOptionsForRequest(method, protocol, host, port, path, qs) {
+
+    const options = {
+        method: method,
+        url: protocol + '://' + host + '/' + path,
+        qs: qs,
+        json: true,
+        headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+        },
+    };
+
+    try {
+        options.pfx = fs.readFileSync(p12File);
+        options.passphrase = CONFIG.LICENSE_CENTRAL.CERT.PASS_PHRASE;
+        options.ca = fs.readFileSync(caFile);
+    }
+    catch (err) {
+        logger.warn('[license_central_adapter] Error loading certificates');
+        logger.info(err);
+    }
+
+    return options;
+}
+
+self.createItem = function (itemId, itemName, productCode, callback) {
+
+    if (typeof(callback) !== 'function') {
+        callback = function () {
+            logger.info('[license_central_adapter] Callback not registered');
+        }
+    }
+
+    const options = buildOptionsForRequest(
+        'POST',
+        CONFIG.HOST_SETTINGS.LICENSE_CENTRAL.PROTOCOL,
+        CONFIG.HOST_SETTINGS.LICENSE_CENTRAL.HOST,
+        CONFIG.HOST_SETTINGS.LICENSE_CENTRAL.PORT,
+        'doCreateItem.php',
+        {}
+    );
+
+    options.body = {
+        itemid: itemId,
+        itemname: itemName,
+        firmcode: CONFIG.LICENSE_CENTRAL.FIRM_CODE,
+        productcode: productCode
+    };
+
+    request(options, function (e, r, message) {
+        logger.log(message);
+        const err = logger.logRequestAndResponse(e, options, r, message);
+
+        if (err) {
+            return callback(err);
+        }
+
+        callback(err, true)
+    });
+
+};
+
+self.encryptData = function (productCode, data, callback) {
+    if (typeof(callback) !== 'function') {
+        callback = function () {
+            logger.info('[license_central_adapter] Callback not registered');
+        }
+    }
+
+    const options = buildOptionsForRequest(
+        'POST',
+        CONFIG.HOST_SETTINGS.LICENSE_CENTRAL.PROTOCOL,
+        CONFIG.HOST_SETTINGS.LICENSE_CENTRAL.HOST,
+        CONFIG.HOST_SETTINGS.LICENSE_CENTRAL.PORT,
+        'doEncryptData.php',
+        {}
+    );
+
+    options.body = {
+        firmcode: CONFIG.LICENSE_CENTRAL.FIRM_CODE,
+        productcode: productCode,
+        buffer: data
+    };
+
+    request(options, function (e, r, message) {
+        const err = logger.logRequestAndResponse(e, options, r, message);
+
+        if (err) {
+            return callback(err);
+        }
+
+        callback(err, message.content);
+    });
+};
+
+self.createAndActivateLicense = function (hsmId, customerId, itemId, quantity, callback) {
+    if (typeof(callback) !== 'function') {
+        callback = function () {
+            logger.info('[license_central_adapter] Callback not registered');
+        }
+    }
+
+    const options = buildOptionsForRequest(
+        'POST',
+        CONFIG.HOST_SETTINGS.LICENSE_CENTRAL.PROTOCOL,
+        CONFIG.HOST_SETTINGS.LICENSE_CENTRAL.HOST,
+        CONFIG.HOST_SETTINGS.LICENSE_CENTRAL.PORT,
+        'doCreateOrder.php',
+        {}
+    );
+    options.body = {
+        cmserial: hsmId,
+        customerid: customerId,
+        itemid: itemId,
+        quantity: quantity
+    };
+
+    request(options, function (e, r, message) {
+        const err = logger.logRequestAndResponse(e, options, r, message);
+
+        if (err) {
+            return callback(err);
+        }
+
+        callback(err, true)
+    });
+};
+
+self.doLicenseUpdate = function (hsmId, context, callback) {
+    if (typeof(callback) !== 'function') {
+        callback = function () {
+            logger.info('[license_central_adapter] Callback not registered');
+        }
+    }
+
+    const options = buildOptionsForRequest(
+        'POST',
+        CONFIG.HOST_SETTINGS.LICENSE_CENTRAL.PROTOCOL,
+        CONFIG.HOST_SETTINGS.LICENSE_CENTRAL.HOST,
+        CONFIG.HOST_SETTINGS.LICENSE_CENTRAL.PORT,
+        'doLicenseUpdate.php',
+        {}
+    );
+
+    options.body = {
+        cmserial: hsmId,
+        context: context,
+        firmcode: CONFIG.LICENSE_CENTRAL.FIRM_CODE,
+        cmactid: CONFIG.LICENSE_CENTRAL.CMACTID,
+        lcaction: CONFIG.LICENSE_CENTRAL.LCACTION
+    };
+
+    request(options, function (e, r, message) {
+        let isOutOfDate = false;
+
+        const err = logger.logRequestAndResponse(e, options, r, message);
+
+        if (err) {
+            return callback(err);
+        }
+
+        // 205 indicates that the provided context is out of date.
+        // The context within the lcc is different
+        // After applying the sent update, the client needs to request a update again, using a new context.
+        if (r.statusCode === 205) {
+            isOutOfDate = true;
+        }
+
+        callback(err, message.content, isOutOfDate);
+    });
+};
+
+self.createAndEncrypt = function (itemId, itemName, productCode, data, callback) {
+    if (typeof(callback) !== 'function') {
+        callback = function () {
+            logger.info('[license_central_adapter] Callback not registered');
+        }
+    }
+
+    self.createItem(itemId, itemName, productCode, function (err, success) {
+
+        if (err) {
+            logger.crit('[license_central_adapter] Could not create item');
+            return callback(err);
+        }
+
+        if (!success) {
+            return callback(new Error('[license_central_adapter] Something broke without error message'));
+        }
+
+        self.encryptData(productCode, data, function (err, encryptedData) {
+            if (err) {
+                logger.crit('[license_central_adapter] Could not encrypt data');
+                return callback(err);
+            }
+
+            return callback(null, encryptedData);
+        })
+    })
+};
+
+self.doConfirmUpdate = function (context, callback) {
+    if (typeof(callback) !== 'function') {
+        callback = function () {
+            logger.info('[license_central_adapter] Callback not registered');
+        }
+    }
+
+    const options = buildOptionsForRequest(
+        'POST',
+        CONFIG.HOST_SETTINGS.LICENSE_CENTRAL.PROTOCOL,
+        CONFIG.HOST_SETTINGS.LICENSE_CENTRAL.HOST,
+        CONFIG.HOST_SETTINGS.LICENSE_CENTRAL.PORT,
+        'doConfirmUpdate.php',
+        {}
+    );
+
+    options.body = {
+        context: context
+    };
+
+    request(options, function (e, r, message) {
+        const err = logger.logRequestAndResponse(e, options, r, message);
+
+        if (err) {
+            return callback(err);
+        }
+
+        callback(null);
+    });
+};
+module.exports = self;
---
+++ MarketplaceCore-app/adapter/license_central_strategy/docker.js
@@ -0,0 +1,26 @@
+
+let self = require('./default');
+
+logger.warn('[license_central_adapter] RUNNING DOCKER (SIMULATION) MODE. LCC calls will be skipped!');
+
+self.createItem = function (itemId, itemName, productCode, callback) {
+    callback(null, true);
+};
+
+self.encryptData = function (productCode, data, callback) {
+    callback(null, data);
+};
+
+self.createAndActivateLicense = function (hsmId, customerId, itemId, quantity, callback) {
+    callback(null, true);
+};
+
+self.doLicenseUpdate = function (hsmId, context, callback) {
+    callback(null, new Buffer('SIMULATION').toString('base64'));
+};
+
+self.doConfirmUpdate = function (context, callback) {
+    callback(null);
+};
+
+module.exports = self;
