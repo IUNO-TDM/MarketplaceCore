b1c02ae49809f0866ddf614fb7354c27eedfae08
Christian Görg <christian.goerg@de.trumpf.com>
2018-01-08 13:10:31 +0100

    Payout validation scheme, payout error handling

--- MarketplaceCore-app/schema/vault_schema.js
+++ MarketplaceCore-app/schema/vault_schema.js
@@ -9,30 +9,32 @@ self.Empty = {
 self.Payout = {
     type: 'object',
     properties: {
-
+        payoutId:{
+            type: 'string',
+            format: 'uuid'
+        },
         payoutAddress:{
             type: 'string',
-            pattern: '[a-zA-Z1-9]{27,35}$',
-            required: true
+            pattern: '[a-zA-Z1-9]{27,35}$'
         },
         amount:{
             type: 'integer',
             minimum: 1,
-            maximum: 100000000,
-            required: true
+            maximum: 100000000
         },
         emptyWallet:{
-            type: 'boolean',
-            required: true
+            type: 'boolean'
         },
         referenceId:{
             type: 'string',
-            format: 'alphanumeric',
-            required: false
+            maxLength: 50
 
         }
     },
-    additionalProperties: false
+    additionalProperties: false,
+    required: [
+        'amount', 'payoutAddress', 'emptyWallet'
+    ]
 };
 
 module.exports = self;
