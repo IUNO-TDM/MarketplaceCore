f7b45e71e7c6b9a347ded06d7ddfb477a3dd6b78
TRUMPF\ElyGomesMa <marcel.elygomes@de.trumpf.com>
2018-01-11 10:39:13 +0100

    HotFix nr. 2 for SqlInjection Security Issue in CreateLog Function

--- database/patches/iuno_marketplacecore_V0016V_20180110.sql
+++ database/patches/iuno_marketplacecore_V0016V_20180110.sql
@@ -64,36 +64,8 @@ $$
         RETURNS void
         LANGUAGE 'plpgsql'
     AS $BODY$
-
-          DECLARE vLogID integer:= (select nextval('LogID'));
-              vSqlCmd varchar :=  concat(
-                                            'INSERT INTO LogTable(LogID, LogStatusID, LogMessage, LogObjectName, Parameters,CreatedAt)',
-                                            'VALUES( ',
-                                            cast(vLogID as varchar),
-                                            ', ',
-                                            cast(vLogStatusID as varchar),
-                                            ', ''',
-                                            vLogMessage,
-                                            ''', ''',
-                                            vLogObjectName,
-                                            ''', ''',
-                                            vParameters,
-                                            ''', ''',
-                                            now(),
-                                            ''')'
-                                       );
-
-             vConnName text := 'conn';
-             vConnString text := 'dbname=oauthdb port=5432 host=localhost user=oauthdb_loguser password=PASSWORD';
-             vConnExist bool := (select ('{' || vConnName || '}')::text[] <@ (select dblink_get_connections()));
           BEGIN
 
-            if(not vConnExist or vConnExist is null) then
-                    perform dblink_connect(vConnName,vConnString);
-            end if;
-                    perform dblink(vConnName,vSqlCmd);
-                    perform dblink_disconnect(vConnName);
-                    set role postgres;
           END;
 
     $BODY$;
