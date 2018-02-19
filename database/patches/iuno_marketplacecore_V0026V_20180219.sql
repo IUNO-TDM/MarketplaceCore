98fc0485953fc8bbf4888759c4174e68f290a9e2
TRUMPF\ElyGomesMa <marcel.elygomes@de.trumpf.com>
2018-02-19 15:25:35 +0100

    #127 updated rolespermissions. Admin users are allowed to to call any function

---
+++ database/patches/iuno_marketplacecore_V0026V_20180219.sql
@@ -0,0 +1,72 @@
+﻿--#######################################################################################################
+--TRUMPF Werkzeugmaschinen GmbH & Co KG
+--TEMPLATE FOR DATABASE PATCHES, HOT FIXES and SCHEMA CHANGES
+--Author: Marcel Ely Gomes
+--CreateAt: 2018-02-19
+--Version: 00.00.01 (Initial)
+--#######################################################################################################
+-- READ THE INSTRUCTIONS BEFORE CONTINUE - USE ONLY PatchDBTool to deploy patches to existing Databases
+-- Describe your patch here
+-- Patch Description:
+-- 	1) Why is this Patch necessary?
+-- 	2) Which Git Issue Number is this patch solving?
+-- 	3) Which changes are going to be done?
+-- PATCH FILE NAME - THIS IS MANDATORY
+-- iuno_<databasename>_V<patchnumber>V_<creation date>.sql
+-- PatchNumber Format: 00000 whereas each new Patch increase the patchnumber by 1
+-- Example: iuno_marketplacecore_V00001V_20170913.sql
+--#######################################################################################################
+-- PUT YOUR STATEMENTS HERE:
+-- 	1) Why is this Patch necessary?
+--  Allow Admin users to call any function
+-- 	2) Which Git Issue Number is this patch solving?
+--  #127
+-- 	3) Which changes are going to be done?
+--  Insert permissions
+--: Run Patches
+------------------------------------------------------------------------------------------------
+--##############################################################################################
+-- Write into the patch table: patchname, patchnumber, patchdescription and start time
+--##############################################################################################
+DO
+$$
+	DECLARE
+		PatchName varchar		 	 := 'iuno_marketplacecore_V0026V_20180219';
+		PatchNumber int 		 	 := 0026;
+		PatchDescription varchar 	 := 'Allow Admin users to call any function.';
+		CurrentPatch int 			 := (select max(p.patchnumber) from patches p);
+
+	BEGIN
+		--INSERT START VALUES TO THE PATCH TABLE
+		IF (PatchNumber <= CurrentPatch) THEN
+			RAISE EXCEPTION '%', 'Wrong patch number. Please verify your patches!';
+		ELSE
+			INSERT INTO PATCHES (patchname, patchnumber, patchdescription, startat) VALUES (PatchName, PatchNumber, PatchDescription, now());
+		END IF;
+	END;
+$$;
+------------------------------------------------------------------------------------------------
+--##############################################################################################
+-- Run the patch itself and update patches table
+--##############################################################################################
+DO
+$$
+		DECLARE
+			vPatchNumber int := 0026;
+		BEGIN
+-- #########################################################################################################################################
+
+     insert into rolespermissions (roleid, functionid)
+     select 0, functionid from functions where functionid not in
+     (select functionid from rolespermissions
+     where roleid = 0);
+
+----------------------------------------------------------------------------------------------------------------------------------------
+    -- UPDATE patch table status value
+    UPDATE patches SET status = 'OK', endat = now() WHERE patchnumber = vPatchNumber;
+    --ERROR HANDLING
+    EXCEPTION WHEN OTHERS THEN
+        UPDATE patches SET status = 'ERROR: ' || SQLERRM || ' ' || SQLSTATE || 'while creating patch.'	WHERE patchnumber = vPatchNumber;
+     RETURN;
+END;
+$$;
