99ef93829954ef133bb77d701c306a30c61521b8
TRUMPF\ElyGomesMa <marcel.elygomes@de.trumpf.com>
2018-03-15 18:11:30 +0100

    Create Database call for Protocols

--- MarketplaceCore-app/routes/protocols.js
+++ MarketplaceCore-app/routes/protocols.js
@@ -5,7 +5,7 @@ const {Validator, ValidationError} = require('express-json-validator-middleware'
 const validator = new Validator({allErrors: true});
 const validation_schema = require('../schema/protocol_schema');
 const validate = validator.validate;
-
+const dbProtocols = require('../database/function/protocol');
 
 router.post('/:clientId', validate({
     query: validation_schema.Empty,
@@ -15,6 +15,14 @@ router.post('/:clientId', validate({
     let protocol = req.body;
     console.log("Protocol of type \""+ protocol.eventType+ "\" received from " +clientId);
     //TODO: Save protocol to db
+    dbProtocols.CreateProtocols(protocol, clientId, req.token.user.roles, function (err, data) {
+        if (err) {
+            next(err);
+        }
+        else {
+            res.json(data);
+        }
+    });
 });
 
 module.exports = router;
