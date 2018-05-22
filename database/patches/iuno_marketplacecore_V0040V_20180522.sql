6a56305d510f2d2ea229882bea4dafbf16247fe2
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2018-05-22 15:50:17 +0200

    added missing db patch to track api requests close #184

---
+++ database/patches/iuno_marketplacecore_V0040V_20180522.sql
@@ -0,0 +1,80 @@
+﻿--#######################################################################################################
+--TRUMPF Werkzeugmaschinen GmbH & Co KG
+--TEMPLATE FOR DATABASE PATCHES, HOT FIXES and SCHEMA CHANGES
+--Author: Manuel Beuttler
+--CreateAt: 2018-05-22
+--Version: 00.00.01 (Initial)
+--#######################################################################################################
+-- READ THE INSTRUCTIONS BEFORE CONTINUE - USE ONLY PatchDBTool to deploy patches to existing Databases
+-- Describe your patch here
+-- Patch Description:
+-- 	1) Why is this Patch necessary?
+-- 	2) Which Git Issue Number is this patch solving?
+-- 	3) Which changes are going to be done?
+-- PATCH FILE NAME - THIS IS MANDATORY
+-- iuno_<databasename>_V<patchnumber>V_<creation date>.sql
+-- PatchNumber Format: 00000 whereas each new Patch increase the patchnumber by 1
+-- Example: iuno_marketplacecore_V00001V_20170913.sql
+--#######################################################################################################
+-- PUT YOUR STATEMENTS HERE:
+-- 	1) Why is this Patch necessary?
+--  A miss use of our API can lead to DDOS attack by ingesting endless data into our database
+-- 	2) Which Git Issue Number is this patch solving?
+--  #184
+-- 	3) Which changes are going to be done?
+--  Adding a new table to log request information and allow brute-force protection in the backend
+--: Run Patches
+------------------------------------------------------------------------------------------------
+--##############################################################################################
+-- Write into the patch table: patchname, patchnumber, patchdescription and start time
+--##############################################################################################
+DO
+$$
+	DECLARE
+		PatchName varchar		 	 := 'iuno_marketplacecore_V0040V_20180522';
+		PatchNumber int 		 	 := 0040;
+		PatchDescription varchar 	 := 'Adding a new table to log request information and allow brute-force protection in the backend';
+		CurrentPatch int 			 := (select max(p.patchnumber) from patches p);
+
+	BEGIN
+		--INSERT START VALUES TO THE PATCH TABLE
+		IF (PatchNumber <= CurrentPatch) THEN
+			RAISE EXCEPTION '%', 'Wrong patch number. Please verify your patches!';
+		ELSE
+			INSERT INTO PATCHES (patchname, patchnumber, patchdescription, startat) VALUES (PatchName, PatchNumber, PatchDescription, now());
+		END IF;
+	END;
+$$;
+------------------------------------------------------------------------------------------------
+--##############################################################################################
+-- Run the patch itself and update patches table
+--##############################################################################################
+DO
+$$
+		DECLARE
+			vPatchNumber int := 0040;
+		BEGIN
+-- #########################################################################################################################################
+
+CREATE TABLE brute
+(
+    id                  text NOT NULL,
+    count               int NOT NULL,
+    first_request       timestamp with time zone NOT NULL,
+    last_request        timestamp with time zone NOT NULL,
+    expires             timestamp with time zone NOT NULL
+);
+ALTER TABLE brute ADD CONSTRAINT brute_PK PRIMARY KEY (id);
+
+-- #########################################################################################################################################
+
+
+----------------------------------------------------------------------------------------------------------------------------------------
+    -- UPDATE patch table status value
+    UPDATE patches SET status = 'OK', endat = now() WHERE patchnumber = vPatchNumber;
+    --ERROR HANDLING
+    EXCEPTION WHEN OTHERS THEN
+        UPDATE patches SET status = 'ERROR: ' || SQLERRM || ' ' || SQLSTATE || 'while creating patch.'	WHERE patchnumber = vPatchNumber;
+     RETURN;
+END;
+$$;
